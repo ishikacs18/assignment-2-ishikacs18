dba5e73b63c2a0a5439823d36d5022e7361469f2
ishikasinghcs18 <ishikasinghcs18>
2020-03-31 11:29:58 +0530

    added

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -5,6 +5,66 @@
  *  Time: 7:15 PM
  */
 package problem3.myqueue;
-
+import problem3.myLinkedList.MyLinkedList;
+import problem3.student.Student;
 public class MyPriorityQueue {
+	 MyLinkedList linkedList = new MyLinkedList();
+	    public void enqueue(Student student){
+	        linkedList.addLast(student);
+	    }
+
+	    public void dequeue(){
+	        linkedList.deleteFirst();
+	    }
+	    public void show(){
+	        linkedList.display();
+    }
+}
+package problem3.myLinkedList;
+
+import problem3.node.Node;
+import problem3.student.Student;
+
+public class MyLinkedList {
+    private Node first;
+    private Node last;
+
+    public void addLast(Student student){
+        Node node = new Node(student);
+        if(first==null){
+            first=last=node;
+            return;
+        }
+        last.setNext(node);
+        last=node;
+
+        var current = first;
+        while(current!=null){
+            if(node.getData().getRollNo()<current.getData().getRollNo()) {
+                var temp = node.getData();
+                node.setData(current.getData());
+                current.setData(temp);
+            }
+            current= current.getNext();
+        }
+    }
+
+    public void deleteFirst(){
+        if(first==null){
+            first=last=null;
+            return;
+        }
+        var second = first.getNext();
+        first.setNext(null);
+        first=second;
+    }
+
+    public void display(){
+        var current = first;
+        while(current!=null){
+            System.out.println(current.getData());
+            current=current.getNext();
+        }
+    }
+
 }
--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,69 @@
  */
 package problem3.node;
 
+import problem3.student.Student;
 public class Node {
+	  private Student data;
+	    private Node next;
+
+	    public Node(Student data){
+	        this.data=data;
+	        this.next=null;
+	    }
+
+	    public Student getData() {
+	        return data;
+	    }
+
+	    public void setData(Student data) {
+	        this.data = data;
+	    }
+
+	    public Node getNext() {
+	        return next;
+	    }
+
+	    public void setNext(Node next) {
+	        this.next = next;
+    }
 }
+package problem3.student;
+
+public class Student {
+    private String name;
+    private int rollNo;
+
+    public Student(String name,int rollNo){
+        this.name=name;
+        this.rollNo=rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String toString(){
+        return getName()+" "+getRollNo();
+    }
+}
+
+
+
+
+
+
+
+
+
